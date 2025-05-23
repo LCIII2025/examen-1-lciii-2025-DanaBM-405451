e277f80b8656af85a16325ffc4c9b79fef5cb5ac
DanaBM-405451 <405451@tecnicatura.frc.utn.edu.ar>
2025-05-22 21:17:52 -0300

    Ni idea, un test paso y el otro no... pero se q no funciona ninguno

--- src/test/java/org/example/parking/EstacionamientoTest.java
+++ src/test/java/org/example/parking/EstacionamientoTest.java
@@ -12,34 +12,41 @@ public class EstacionamientoTest {
     @Test
     public void testRetirarVehiculo() throws Exception {
         //TODO test
-        Cliente persona = new Cliente("11122233", "Nico Robin")
+        Cliente persona = new Cliente("11122233", "Nico Robin");
         Vehiculo vehi = new Vehiculo("456zxc", "march", Vehiculo.Tipo.AUTO);
         LocalDateTime entrada = LocalDateTime.MIN;
         LocalDateTime salida = LocalDateTime.MAX;
 
-        Ticket tick = new Ticket(persona, vehi, entrada, salida);
-        assert true;
-        assert tick.canEqual(persona);
-        throw new Exception("");
-        testRetirarVehiculo();
 
-    }
 
-        /*La verdad profe ni idea de como se hacen los test...
+        Ticket tick = new Ticket(persona, vehi);
+
+        assertEquals("Se encontro vehiculo",vehi.getPatente());
+
+         /*La verdad profe ni idea de como se hacen los test...
+         No entiendo porque no me pasa....
         osea creo q lo inicie pero no estoy segura de como testear*/
 
+
     }
+
+
+
+
 
     @Test
     public void testCalcularPrecio() throws Exception {
         // TODO test
-        Cliente persona2 = new Cliente("11122233","Nico Robin")
+        Cliente persona2 = new Cliente("11122233","Nico Robin");
         Vehiculo vehi2 = new Vehiculo("456zxc","march", Vehiculo.Tipo.AUTO);
         LocalDateTime entrada2 = LocalDateTime.MIN;
         LocalDateTime salida2 = LocalDateTime.MAX;
 
-       testCalcularPrecio();
-       //Aqui lo mismo.. se que debo usar assert pero hasta ahi
+
+
+       /*Aqui lo mismo...
+       Aunque este me paso y estoy segura que asi no era
+       se que debo usar assert pero hasta ahi*/
 
     }
 
